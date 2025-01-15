a18bdbbcc5cec22b9406c3b7901212b60831d28f
Ek0n13 <alextgen@gmail.com>
2025-01-15 13:24:36 +0200

    esm conversion attempt

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./src/**/*.{html,js,jsx,ts,tsx}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+export const content = ['./src/**/*.{html,js,jsx,ts,tsx}'];
+export const theme = {
+  extend: {},
+};
+export const plugins = [];
 
+// module.exports = {
+//   content: ['./src/**/*.{html,js,jsx,ts,tsx}'],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
